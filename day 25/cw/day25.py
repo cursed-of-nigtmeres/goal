9c32c95300a1f492c0cd19fbe9849c70b0a33e08
danieli javaxidze <danielijavaxidze@gmail.com>
2024-12-11 18:17:30 +0400

    day 26

---
+++ day 25/cw/day25.py
@@ -0,0 +1,17 @@
+N2 = ("  ")
+
+names = ["nirvana", "type o negative", "deftones", "slipknot", "cannibal corpse"]
+names.insert(4, "limp bizikit")
+print(names)
+
+N3 = (" ")
+
+nums = ["0", "1", "2", "3", "4"]
+nums.pop(0)
+
+print(nums)
+
+N4 = (" ")
+
+nums2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+nums2.pop(str(input("enter num for 0 to 9: ")))
